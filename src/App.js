afe208143c3796752150dd79d4fc16adcb5fc5dd
Eric Baker <ericbaker1995@gmail.com>
2022-03-29 20:33:35 -0400

    Init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  /* Vanilla Javascript
+  const para = document.createElement("p");
+  para.textContent = "This is visable 2";
+  */
+  document.getElementById("root");
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
